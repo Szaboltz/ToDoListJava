b5db8625c65fa972c8912fa6eec10b234ce1b0de
Henrique Szabo <118364512+Szaboltz@users.noreply.github.com>
2023-10-15 05:52:54 -0300

    Add files via upload

--- src/main/java/br/com/szabohenrique/todolist/filter/FilterTaskAuth.java
+++ src/main/java/br/com/szabohenrique/todolist/filter/FilterTaskAuth.java
@@ -27,7 +27,7 @@ public class FilterTaskAuth extends OncePerRequestFilter {
 
                 var serveletPath = request.getServletPath();
 
-                if (serveletPath.equals("/tasks/")) {
+                if (serveletPath.startsWith("/tasks/")) {
 
                     // pegar autenticação (usuário e senha)
                     var authorization = request.getHeader("Authorization");
--- src/main/java/br/com/szabohenrique/todolist/task/TaskController.java
+++ src/main/java/br/com/szabohenrique/todolist/task/TaskController.java
@@ -3,18 +3,18 @@ package br.com.szabohenrique.todolist.task;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-import org.hibernate.mapping.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.szabohenrique.todolist.utils.Utils;
 import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
@@ -53,7 +53,23 @@ public class TaskController {
         return tasks;
     }
 
-    public void update(@RequestBody TaskModel taskModel, HttpServletRequest request) {
-        
+    @PutMapping("/{id}")
+    public ResponseEntity update(@RequestBody TaskModel taskModel, HttpServletRequest request, @PathVariable UUID id) {
+        var task = this.taskRepository.findById(id).orElse(null);
+
+        if (task == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Tarefa não encontrada");
+        }
+
+        var idUser = request.getAttribute("idUser");
+
+        if(!task.getIdUser().equals(idUser)) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário não tem permissão para alterar essa tarefa");
+        }
+
+        Utils.copyNonNullProperties(taskModel, task);
+
+        var taskUpdated = this.taskRepository.save(task);
+        return ResponseEntity.ok().body(taskUpdated);
     }
 }
